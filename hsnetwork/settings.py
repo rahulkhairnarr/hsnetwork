7a4b9b23e3435e7854d06618fac88c88b6d32b4c
Rahul Khairnar <rahulkhairnar@aumtelecom.com>
2020-10-08 12:20:26 +0530

    DB

--- hsnetwork/settings.py
+++ hsnetwork/settings.py
@@ -95,8 +95,12 @@ WSGI_APPLICATION = 'hsnetwork.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+                'USER': 'hobnob_admin',
+                'PASSWORD': 'hobnobaum',
+                'HOST': 'hobnobaum.cbsyniih1ku9.ap-south-1.rds.amazonaws.com',
+                'PORT': '5432',
     }
 }
 
@@ -138,6 +142,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
+
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
